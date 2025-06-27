30bc92afa17b15b11256e1a27d569b5d999dc183
asadkaptech <163139210+asadklm@users.noreply.github.com>
2025-06-28 00:03:12 +0500

    Broke Template and made content dynamic

--- admin/db/sep_dism.sql
+++ admin/db/sep_dism.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2025 at 02:00 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Jun 27, 2025 at 09:02 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,7 +31,16 @@ CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(256) NOT NULL,
   `category_image` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`, `category_image`) VALUES
+(2, 'Women', 'womenCategory.jpg'),
+(4, 'Best Accessories ', 'accessoriesCategory.jpg'),
+(5, 'Men', 'manUpdateCategory.jpg');
 
 -- --------------------------------------------------------
 
@@ -44,7 +53,15 @@ CREATE TABLE `orders` (
   `order_generated_id` int(11) NOT NULL,
   `order_amount` int(11) NOT NULL,
   `order_status` varchar(256) NOT NULL DEFAULT 'pending'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `order_generated_id`, `order_amount`, `order_status`) VALUES
+(1, 115, 2500, 'shipped'),
+(2, 220, 2299, 'pending');
 
 -- --------------------------------------------------------
 
@@ -57,10 +74,17 @@ CREATE TABLE `products` (
   `product_name` varchar(256) NOT NULL,
   `product_description` varchar(256) NOT NULL,
   `product_price` varchar(256) NOT NULL,
-  `product_category` int(11) NOT NULL,
+  `product_category_id` int(11) NOT NULL,
   `product_stock_quantity` varchar(256) NOT NULL,
   `product_image` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `product_description`, `product_price`, `product_category_id`, `product_stock_quantity`, `product_image`) VALUES
+(2, 'Esprit Ruffle Shirt', 'Elevate your wardrobe with the timeless elegance of the Esprit Ruffle Shirt. Crafted from soft, breathable fabric, this shirt combines comfort with a touch of sophistication. ', '900', 2, '2', 'product-01.jpg');
 
 --
 -- Indexes for dumped tables
@@ -92,19 +116,19 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
