ac397595eef005c8e79ec4a5fe8a386f529ac3af
Asad Khan <asadkaptech@gmail.com>
2025-07-01 17:22:29 +0500

    DB

--- admin/db/sep_dism.sql
+++ admin/db/sep_dism.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2025 at 02:28 PM
+-- Generation Time: Jul 01, 2025 at 02:21 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -66,9 +66,11 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`order_id`, `order_generated_id`, `order_amount`, `order_status`, `customer_id`, `customer_name`, `customer_number`, `order_shipping_address`, `order_placed_date`) VALUES
 (1, 'ORD-68627688391f0', 1800, 'shipped', 1, 'Asad Khan', '1231354', 'abc', '2025-06-30 12:20:24'),
-(2, 'ORD-68627702eae5b', 1299, 'pending', 1, 'Asad Khan', '6126526', 'abc', '2025-06-30 11:37:38'),
+(2, 'ORD-68627702eae5b', 1299, 'delivered', 1, 'Asad Khan', '6126526', 'abc', '2025-07-01 11:56:08'),
 (3, 'ORD-6862806a317ae', 1800, 'pending', 1, 'Asad Khan', '778672', 'vagv', '2025-06-30 12:17:46'),
-(4, 'ORD-686280b91f810', 1800, 'pending', 1, 'Asad Khan', '626', 'abc', '2025-06-30 12:19:05');
+(4, 'ORD-686280b91f810', 1800, 'pending', 1, 'Asad Khan', '626', 'abc', '2025-06-30 12:19:05'),
+(5, 'ORD-6863cb672922c', 900, 'pending', 3, 'Kinza', '4544111', 'Governor House KHI', '2025-07-01 11:49:59'),
+(6, 'ORD-6863cc172d74a', 3098, 'pending', 3, 'Kinza', '545464554', 'ABC', '2025-07-01 11:52:55');
 
 -- --------------------------------------------------------
 
@@ -91,9 +93,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `product_description`, `product_price`, `product_category_id`, `product_stock_quantity`, `product_image`) VALUES
-(2, 'Esprit Ruffle Shirt', 'Elevate your wardrobe with the timeless elegance of the Esprit Ruffle Shirt. Crafted from soft, breathable fabric, this shirt combines comfort with a touch of sophistication. ', '900', 2, 48, 'product-01.jpg'),
-(3, 'Herschel supply', 'Herschel Supply is renowned for its timeless blend of classic style and modern functionality. Crafted with high-quality materials and attention to detail, Herschel backpacks and accessories offer both durability and a sleek, minimalist design that suits ev', '1299', 2, 20, 'product-02.jpg'),
-(4, 'Vintage Inspired Classic', 'Vintage Inspired Classic pieces bring a timeless charm that merges nostalgic design with modern-day quality. These products capture the essence of past eras with carefully crafted details, muted tones, and durable materials that stand the test of time. ', '1799', 5, 10, 'product-06.jpg'),
+(2, 'Esprit Ruffle Shirt', 'Elevate your wardrobe with the timeless elegance of the Esprit Ruffle Shirt. Crafted from soft, breathable fabric, this shirt combines comfort with a touch of sophistication. ', '900', 2, 47, 'product-01.jpg'),
+(3, 'Herschel supply', 'Herschel Supply is renowned for its timeless blend of classic style and modern functionality. Crafted with high-quality materials and attention to detail, Herschel backpacks and accessories offer both durability and a sleek, minimalist design that suits ev', '1299', 2, 19, 'product-02.jpg'),
+(4, 'Vintage Inspired Classic', 'Vintage Inspired Classic pieces bring a timeless charm that merges nostalgic design with modern-day quality. These products capture the essence of past eras with carefully crafted details, muted tones, and durable materials that stand the test of time. ', '1799', 5, 9, 'product-06.jpg'),
 (5, 'Checked Shirt', 'The Checked Shirt is a timeless wardrobe staple that effortlessly blends casual comfort with classic style. Featuring a versatile checkered pattern, it adds a touch of rustic charm to any outfit. Made from soft, breathable fabric, this shirt is perfect for', '1799', 5, 0, 'product-11.jpg');
 
 -- --------------------------------------------------------
@@ -115,9 +117,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_role`) VALUES
-(1, 'Asad Khan', 'asad@gmail.com', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 1),
-(2, 'Talib Hussain', 'talib@gmail.com', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 1),
-(4, 'admin', 'admin@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 0);
+(1, 'ali', 'ali@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1),
+(2, 'hassan', 'hassan@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 1),
+(3, 'Kinza', 'kinza@gmail.com', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 0);
 
 --
 -- Indexes for dumped tables
@@ -161,7 +163,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -173,7 +175,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
